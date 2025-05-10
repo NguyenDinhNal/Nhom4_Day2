81cfb345f0362f01974da822e14542d05434cf94
NguyenDinhNal <phoney2k3@gmail.com>
2025-05-10 10:33:16 +0700

    Nhom4_day2

---
+++ Debug/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.d
@@ -0,0 +1,18 @@
+SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.o: \
+ C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.c \
+ C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h \
+ C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/utilities.h \
+ C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h
+C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h:
+C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/utilities.h:
+C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+C:/Users/USER/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
